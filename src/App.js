1d5be87d6829a4978f4591f41bcffd39a6d05e18
harsh9975 <gautam33776@gmail.com>
2020-11-20 01:30:43 +0530

    router and dashboard setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
-import Signup from './components/Signup'
+import Signup from './components/Signup';
 import { AuthProvider } from './contexts/AuthContext';
-
+import { BrowseRouter as Router, Switch, Route} from 'react-router-dom';
+import Dashboard from './components/Dashboard'
 function App() {
   return (
+    <Router>
     <AuthProvider>
-    <Signup/>
+    <Switch>
+      <Route exact path="/" component={Dashboard}/>
+      <Route path="/signup" component={Signup}/>
+    </Switch>
     </AuthProvider>
+    </Router>
+    
   )
 }
 
